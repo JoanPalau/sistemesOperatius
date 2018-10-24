67e2271d77a46d6cc1c7d20140b77fe11b469a9c
Kekino99 <43566871+Kekino99@users.noreply.github.com>
2018-10-24 23:51:47 +0200

    Generador i loteria acabats!!

--- generador.c
+++ generador.c
@@ -14,19 +14,21 @@ Sergi Simón Balcells
 #include<signal.h>
 #include<time.h>
 
+#include<string.h> /* Testos */
+
+#define COLOR "\x1b[32m"
+#define RESET "\x1b[0m"
+
 void end(int sig);
+void mansigquit(int sig);
 
 int main(int argc, char *argv[])
 {
-	int seed;
-	int num;
 	signal(SIGTERM, end);
+	signal(SIGQUIT, mansigquit);
 	while(1)
 	{
-		read(0, (void *) &seed, sizeof(int));
-		srand(seed);
-		num = rand() % 10;
-		write(1, (void *) &num, sizeof(int) );
+		pause();
 	}
 	return -1;
 }
@@ -35,4 +37,25 @@ void end(int sig) {
 	close(0);
 	close(1);
 	exit(0);
+}
+
+void mansigquit(int sig){
+	int seed;
+	int num;
+	char buff[256];
+
+		if(read(0, (void *) &seed, sizeof(int))>=0) {
+			srand(seed);
+			num = rand() % 10;
+			if(write(1, (void *) &num, sizeof(int))<0) {
+				perror("Error en escritura en el pipe");
+			};
+			/* sprintf(buff, "\t%s[%d] Seed: %d Number: %d %s\n", 
+				COLOR, getpid(), seed, num, RESET);
+			write(2, buff, strlen(buff)); 
+			* Debugging commented*/
+		} else {
+			perror("Error de lectura");
+		}
+	signal(SIGQUIT, mansigquit);
 }
